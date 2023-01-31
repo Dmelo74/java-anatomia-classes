364ff1e936ad8933b019b9e362f770ca6527bfe8
DaviMelo <dvdmelo2@yahoo.com.br>
2023-01-31 17:20:14 -0300

    atualização3101

--- src/edu/davi/primeirasemana/BoletimEstudantil.java
+++ src/edu/davi/primeirasemana/BoletimEstudantil.java
@@ -2,7 +2,7 @@ package edu.davi.primeirasemana;
 
 public class BoletimEstudantil {
     public static void main(String[] args) {
-        int mediaFinal = 5;
+        int mediaFinal = 4;
         if (mediaFinal < 6)
             System.out.println("Reprovado");
         else if (mediaFinal == 6)
